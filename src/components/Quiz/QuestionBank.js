20b90050274b3fb900bee0f71055c414a42a9242
puja <pujarani1114@gmail.com>
2022-04-26 12:27:22 +0530

    j

--- src/components/Quiz/QuestionBank.js
+++ src/components/Quiz/QuestionBank.js
@@ -1,60 +1,206 @@
 const QuestionList = [
     {
       question:
-        "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?   ",
-      answers: ["Richard Branson", "Alan Sugar", "Donald Trump", "Bill Gates"],
-      correct: "Alan Sugar",
-      questionId: "099099",
+        "Almatti Dam is on which of the following rivers?   ",
+      answers: ["Krishna", "Kaveri", "Tungabhadra", "Malprabha"],
+      correct: "Krishna",
+      questionId: "1",
     },
     {
       question:
-        'Where is the train station "Llanfair¬pwllgwyngyll¬gogery¬chwyrn¬drobwll-llan¬tysilio¬gogo¬goch"?',
-      answers: ["Wales", "Moldova", "Czech Republic", "Denmark"],
-      correct: "Czech Republic",
-      questionId: "183452",
+        'Which of the following states is India’s largest Bauxite producer"?',
+      answers: ["Odisha", "Jharkhand", "Rajasthan", "Karnataka"],
+      correct: "Jharkhand",
+      questionId: "2",
     },
     {
       question:
-        "Which company did Valve cooperate with in the creation of the Vive?",
-      answers: ["HTC", "Oculus", "Google", "Razer"],
-      correct: "Razer",
-      questionId: "267908",
+        "With which of the following countries, India shares maximum length of border?",
+      answers: ["Pakistan", "China", "Bangladesh", "Nepal"],
+      correct: "Bangladesh",
+      questionId: "3",
     },
     {
-      question: "What's the name of Batman's  parents?",
+      question: "Amaravati, Arkavathy, Bhavani, Chinnar, Hemavati, Honnuhole, Kabini etc. Are tributaries of which among the following rivers?",
       answers: [
-        "Thomas & Martha",
-        "Joey & Jackie",
-        "Jason & Sarah",
-        "Todd & Mira",
+        "Kaveri",
+        "Krishna",
+        "Godavri",
+        "Narmada",
       ],
-      correct: "Jason & Sarah",
-      questionId: "333247",
+      correct: "Kaveri",
+      questionId: "4",
     },
     {
-      question: "What is the most common surname Wales?",
-      answers: ["Jones", "Williams", "Davies", "Evans"],
-      correct: "Evans",
-      questionId: "496293",
+      question: "Amaravati, Arkavathy, Bhavani, Chinnar, Hemavati, Honnuhole, Kabini etc. Are tributaries of which among the following rivers?",
+      answers: ["Sikkim", "Meghalaya", "Arunachal pradesh", "Manipur"],
+      correct: "Arunachal pradesh",
+      questionId: "5",
     },
     {
       question:
-        "What was the name of the WWF professional wrestling tag team made up of the wrestlers Ax and Smash?",
+        "Indravati Hydro Electric Project belongs to which state?",
       answers: [
-        "Demolition",
-        "The Dream Team",
-        "The Bushwhackers",
-        "The British Bulldogs",
+        "Tamil nadu",
+        "Orissa",
+        "Karnataka",
+        "Andhra pradesh",
       ],
-      correct: "The Bushwhackers",
-      questionId: "588909",
+      correct: "Orrisa",
+      questionId: "6",
     },
     {
       question:
-        'What name represents the letter "M" in the NATO phonetic alphabet?',
-      answers: ["Mike", "Matthew", "Mark", "Max"],
-      correct: "Mark",
-      questionId: "648452",
+        'Which among the following is the origin of Damodar river ?',
+      answers: ["Greater Himalaya", "MKumaon Himalaya", "Shayadri Hills", "Chota Nagpur"],
+      correct: "Chota Nagpur",
+      questionId: "7",
+    },
+    {
+      question:
+        "The following hills are present where Eastern Ghats and Western Ghats meet ?   ",
+      answers: ["Cardamom Hills", "Anamalai Hills", "TNilgiri hills", "Shayadri hills"],
+      correct: "Nilgiri Hills",
+      questionId: "8",
+    },
+    {
+      question:
+        "Bring out the only incorrect statement?",
+      answers: [
+        "Maximum Jute Mills in India are in West Bengal",
+        "Maximum Jute Mills in India are in Cooperative Sector",
+        "KMaximum Wool produced in India is of Carpet Grade",
+        "Maximum woolen units in decentralized sector in India are in Punjab",
+      ],
+      correct: "Maximum Jute Mills in India are in Cooperative Sector",
+      questionId: "9",
+    },
+    {
+      question:
+        "Mullaperiyar Dam” has been a long pending issue between which of the following two states?   ",
+      answers: ["Karnataka & Kerala", "KAndhra Pradesh and Tamil Nadu", "Karnataka & Tamil Nadu", "Tamil Nadu and Kerala"],
+      correct: "Tamil Nadu and Kerala",
+      questionId: "10",
+    },
+    {
+      question:
+        "What is the approximate length of the Brahmaputra Plain?   ",
+      answers: ["500 km", "720 km", "T1130 km", "1450 km"],
+      correct: "720 km",
+      questionId: "11",
+    },
+    {
+      question:
+        "Which of the following is the highest peak of the Himalayan range in India?   ",
+      answers: ["K2", "Kanchenjunga", "Nanda Devi", "Mt.Everest"],
+      correct: "Kanchenjunga",
+      questionId: "12",
+    },
+    {
+      question:
+        "Nathu La Pass is situated in which of the following range?   ",
+      answers: ["Vindhyas", "Karakoram", "Western Ghat", "Himalayas"],
+      correct: "Western Ghat",
+      questionId: "13",
+    },
+    {
+      question:
+        "Ananthagiri Hills is located in which of the following states of India?   ",
+      answers: ["Madhya Pradesh", "Telangana", "Chattisgarh", "West Bengal"],
+      correct: "Telangana",
+      questionId: "14",
+    },
+    {
+      question:
+        "Mount Abu is located in which of the following states of India?   ",
+      answers: ["Rajasthan", "Madhya Pradesh", "Uttar Pradesh", "Karnataka"],
+      correct: "Rajasthan",
+      questionId: "15",
+    },
+    {
+      question:
+        "Which of the following is the new name of the Ross island?   ",
+      answers: ["Mahatma Gandhi Island", "Abdul Kalam Island", "Jawaharlal Nehru Island", "Netaji Subhash Chandra Bose Island"],
+      correct: "Netaji Subhash Chandra Bose Island",
+      questionId: "16",
+    },
+    {
+      question:
+        "Which of the following is the longest Drive-In Beach in Asia?   ",
+      answers: ["Muzhappilangad Beach", "Kollam Beach", "Marari beach", "Fort Kochi beach"],
+      correct: "Muzhappilangad Beach",
+      questionId: "17",
+    },
+    {
+      question:
+        "Katchh & Kathiawar Peninsula are separated from each other by which of the following gulfs/ Bays?   ",
+      answers: ["Gulf of Katchh", "Gulf of Khambat", "Gulf of Mannar", "Gulf of Aden"],
+      correct: "Gulf of Katchh",
+      questionId: "18",
+    },
+    {
+      question:
+        "The Bhander Plateau is located in which of the following states of India?   ",
+      answers: ["Madhya Pradesh", "Jharkhand", "Maharashtra", "Karnataka"],
+      correct: "Madhya Pradesh",
+      questionId: "19",
+    },
+    {
+      question:
+        "Which of the following is the northern most island of Nicobar island?   ",
+      answers: ["Little Nicobar", "Car Nicobar", "Great Nicobar", "Teressa island"],
+      correct: "Car Nicobar",
+      questionId: "20",
+    },
+    {
+      question:
+      "Damodar through divided ranchi plateau in the north from which of the following plateau?",
+      answers: ["Koderma Plateau", "Hazaribagh Plateau", "Singhbhum Plateau", "None of the above"],
+      correct: "Hazaribagh Plateau",
+      questionId: "21",
+    },
+    {
+      question:
+        "The Meghalaya Plateau is detached from the Indian peninsula by which of the following?   ",
+      answers: ["Great Boundary Fault", "Himalayan Frontal Fault", "Goranghat gap", "Malda Gap"],
+      correct: "Malda Gap",
+      questionId: "22",
+    },
+    {
+      question:
+        "The southern part of the Karnataka plateau is known as by which of the following name?   ",
+      answers: ["Barkhan", "Malnad", "Maidan", " Hamada"],
+      correct: "Maidan",
+      questionId: "23",
+    },
+  
+    {
+      question:
+        "Sigur Plateau is located in which of the following states of India?   ",
+      answers: ["Karnataka", "Kerala", "Andhra Pradesh", "Tamil Nadu"],
+      correct: "Tamil Nadu",
+      questionId: "24",
+    },
+    {
+      question:
+      " Zuari river is located in which of the following states of India?   ",
+      answers: ["Goa", " Maharashtra", " Kerala", "Arunachal Pradesh"],
+      correct: "Goa",
+      questionId: "25",
+    },
+    {
+      question:
+        "Almatti Dam is on which of the following rivers?   ",
+      answers: ["Krishna", "Kaveri", "Tungabhadra", "Malprabha"],
+      correct: "Krishna",
+      questionId: "1",
+    },
+    {
+      question:
+        "Almatti Dam is on which of the following rivers?   ",
+      answers: ["Krishna", "Kaveri", "Tungabhadra", "Malprabha"],
+      correct: "Krishna",
+      questionId: "1",
     },
   ]
   
